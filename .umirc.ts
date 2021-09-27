2e1ad0d5dfb31969083eb280281ad264e2f13e18
lester <lester@aufs.io>
2021-09-27 15:04:24 +0800

    update title icon

--- .umirc.ts
+++ .umirc.ts
@@ -5,6 +5,9 @@ import theme from "./src/config/theme";
 export default defineConfig({
   routes,
   theme,
+  links: [
+    { rel: 'icon', href: 'https://www.aufs.io/main/wp-content/uploads/2021/09/cropped-aurorafs_logo-03-32x32.png' },
+  ],
   title:"AuFs Dashboard",
   history: {type: "hash"},
   antd: {},
