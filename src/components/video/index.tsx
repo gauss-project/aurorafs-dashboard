785f89d430b7cecac97791f6fd722cc58f9f82ff
lester <lester@aufs.io>
2021-10-18 10:17:33 +0800

    use hls

--- src/components/video/index.tsx
+++ src/components/video/index.tsx
@@ -1,14 +1,12 @@
-import React, { ElementRef, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './index.less';
 import Hls from 'hls.js';
-import videoJs from 'video.js';
-import 'video.js/dist/video-js.css';
 
 import { message } from 'antd';
 
 type Props = {
-  src: string
-}
+  src: string;
+};
 
 const Video: React.FC<Props> = (props) => {
   const ele = useRef<any>(null);
@@ -18,8 +16,8 @@ const Video: React.FC<Props> = (props) => {
       hls.loadSource(props.src);
       hls.attachMedia(ele.current);
       hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        ele.current.play().catch((error: Error) => {
-          //  when an exception is played, the exception flow is followed
+        ele.current.play().catch(() => {
+          message.error('video loading failed');
         });
       });
       hls.on(Hls.Events.ERROR, (event, data) => {
@@ -27,9 +25,11 @@ const Video: React.FC<Props> = (props) => {
       });
     }
   }, []);
-  return <div className={styles.content}>
-    <video id={'video'} ref={ele} width={'100%'} autoPlay controls />
-  </div>;
+  return (
+    <div className={styles.content}>
+      <video id={'video'} ref={ele} width={'100%'} autoPlay controls muted />
+    </div>
+  );
 };
 
 export default Video;
