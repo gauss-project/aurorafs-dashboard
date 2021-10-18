c1279c147caed810a65b15224ce5e92d863c8431
lester <lester@aufs.io>
2021-10-18 10:58:42 +0800

    lazy

--- .umirc.ts
+++ .umirc.ts
@@ -1,26 +1,30 @@
 import { defineConfig } from 'umi';
 
-import routes from "./src/config/routes";
-import theme from "./src/config/theme";
+import routes from './src/config/routes';
+import theme from './src/config/theme';
 export default defineConfig({
   routes,
   theme,
   links: [
-    { rel: 'icon', href: 'https://www.aufs.io/main/wp-content/uploads/2021/09/cropped-aurorafs_logo-03-32x32.png' },
+    {
+      rel: 'icon',
+      href: 'https://www.aufs.io/main/wp-content/uploads/2021/09/cropped-aurorafs_logo-03-32x32.png',
+    },
   ],
-  title:"AuFs Dashboard",
-  history: {type: "hash"},
+  title: 'AuFs Dashboard',
+  history: { type: 'hash' },
   antd: {},
   dva: {
     immer: true,
     hmr: false,
   },
-  nodeModulesTransform: {type:'none'},
+  nodeModulesTransform: { type: 'none' },
   fastRefresh: {},
   ignoreMomentLocale: true,
   webpack5: {},
   targets: {
     ie: 11,
   },
-  publicPath:"./"
+  publicPath: './',
+  mfsu: {},
 });
