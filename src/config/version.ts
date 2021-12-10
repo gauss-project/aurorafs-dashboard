8f310999be6ec662d8a603b941b39542865b2d37
lester <lester@aufs.io>
2021-12-10 16:29:31 +0800

    misc:version 1.1.6

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,4 +1,4 @@
 export const isElectron: boolean =
   window.navigator.userAgent.toLowerCase().indexOf('electron') !== -1;
-export const version: string = '1.1.5';
+export const version: string = '1.1.6';
 export const auroraVersion: string = '1.1.5';
